23ff8b146bee6ec69c889f5501cdecd219b5c1c3
Nirzon Karmakar <nirzon47@gmail.com>
2023-11-11 12:01:42 +0530

    added renderTable()

---
+++ model.js
@@ -0,0 +1,5 @@
+const data = [
+	{ id: 1, fName: 'Nirzon', lName: 'Karmakar', country: 'India', score: '100' },
+	{ id: 2, fName: 'John', lName: 'Doe', country: 'USA', score: '95' },
+	{ id: 3, fName: 'Foo', lName: 'Bar', country: 'UK', score: '90' },
+]
---
+++ script.js
@@ -0,0 +1,37 @@
+// DOM Elements
+const tbody = document.getElementById('tbody')
+
+// Event Listener
+document.addEventListener('DOMContentLoaded', () => {
+	renderTable()
+})
+
+// Functions
+const renderTable = () => {
+	const fragment = document.createDocumentFragment()
+	tbody.innerHTML = ''
+
+	data.forEach((item) => {
+		const tr = document.createElement('tr')
+
+		const trData = `<td>${item.fName} ${item.lName}</td>
+                        <td>${item.country}</td>
+                        <td class="flex gap-x-4 items-center justify-center">
+                            <button class="btn btn-info btn-sm w-12 subtract5">-5</button>
+                            <span>${item.score}</span>
+                            <button class="btn btn-info btn-sm w-12 add5">+5</button>
+                        </td>
+                        <td>
+                            <i
+                                class="fa-solid fa-trash hover:text-red-600 duration-200 cursor-pointer"
+                            ></i>
+                        </td>
+                        `
+
+		tr.id = item.id
+		tr.innerHTML = trData
+		fragment.appendChild(tr)
+	})
+
+	tbody.appendChild(fragment)
+}
